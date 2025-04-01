4af8b63d1c72ff0764bbd6159f677544f4dd90a6
Fry <fryx0r@gmail.com>
2025-04-01 05:01:04 +0000

    Tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Google LLC
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,16 +11,54 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Tests tasks."""
 
-# Note: Use pytest for writing tests!
-import pytest 
+import unittest
+from unittest.mock import patch
 
-# from src.tasks import command
+from src.analyzers.linux_analyzer import (analyze_accts, analyse_shadow_file,
+                                          _extract_linux_credentials)
+from src.analyzers.windows_analyzer import _extract_windows_hashes
 
-def test_task_command():
-    """Test command task."""
+from openrelik_worker_common.reporting import Report, Priority
 
-    ret = "some dummy return value"
-    assert isinstance(ret,str) 
+
+class LinuxAcctTest(unittest.TestCase):
+    """Test the Linux Account analyzer functions."""
+
+    TEST_SHADOW_FILE = "test_data/shadow"
+
+    EXPECTED_CREDENTIALS = {
+        '*':
+        'root',
+        '$6$NS6w5Q6yjrlZiw7s$5jeyNS.bsw2p4nlbbMRI5H8oZnSbbwKs0Lsw94xCouqn/y/yQpKNA4vdPSr/wdA0isyUmq3BD..ZcirwOVNPF/':
+        'testuser'
+    }
+
+    def test_extract_linux_credentials(self):
+        """Tests the extract_linux_credentials method."""
+
+        shadow_file = None
+        with open(self.TEST_SHADOW_FILE, 'r') as data:
+            shadow_file = data.read()
+        credentials = _extract_linux_credentials(shadow_file.split('\n'))
+
+        self.assertEqual(credentials, self.EXPECTED_CREDENTIALS)
+
+
+class WindowsAcctTest(unittest.TestCase):
+    RAW_CREDS = [
+        'testuser:1000:aad3b435b51404eeaad3b435b51404ee:9c7ae0f76b24aad74254914c2b191633:::',
+        'testlocaluser:1004:aad3b435b51404eeaad3b435b51404ee:29f98734e7aa3df2454621ff3928d121:::',
+        'badpassword:1005:aad3b435b51404eeaad3b435b51404ee:7a21990fcd3d759941e45c490f143d5f:::'
+    ]
+    EXPECTED_CREDENTIALS = {
+        '29f98734e7aa3df2454621ff3928d121': 'testlocaluser',
+        '7a21990fcd3d759941e45c490f143d5f': 'badpassword',
+        '9c7ae0f76b24aad74254914c2b191633': 'testuser'
+    }
+
+    def test_extract_windows_hashes(self):
+        creds, credentials = _extract_windows_hashes("test_data/")
+        self.assertDictEqual(credentials, self.EXPECTED_CREDENTIALS)
+        self.assertCountEqual(creds, self.RAW_CREDS)
